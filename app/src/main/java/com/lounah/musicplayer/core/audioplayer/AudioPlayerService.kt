e442c813f92032d445ea1ecc2e4fed5e2b62d639
Lounah <lounahxrussia@gmail.com>
2018-10-08 14:12:43 +0300

    added service-bridge between exoplayer and ui

---
+++ app/src/main/java/com/lounah/musicplayer/core/audioplayer/AudioPlayerService.kt
@@ -0,0 +1,219 @@
+package com.lounah.musicplayer.core.audioplayer
+
+import android.app.Notification
+import com.google.android.exoplayer2.Player
+import android.media.AudioManager
+import android.content.Intent
+import android.content.BroadcastReceiver
+import android.support.v4.media.session.MediaSessionCompat
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.IntentFilter
+import android.graphics.BitmapFactory
+import android.os.*
+import android.support.v4.media.session.PlaybackStateCompat
+import android.util.Log
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.lounah.musicplayer.R
+import com.lounah.musicplayer.presentation.audiotracks.AudioTracksActivity
+
+
+class AudioPlayerService : Service(), AudioManager.OnAudioFocusChangeListener {
+
+    companion object {
+        private val CHANNEL_ID = "MUSIC PLAYER CHANNEL"
+        private val NOTIFICATION_ID = 101
+        private val TAG = AudioPlayerService::class.java.simpleName
+
+        const val MESSAGE_REGISTER_CLIENT = 0
+        const val MESSAGE_UNREGISTER_CLIENT = 1
+        const val MESSAGE_NEXT_TRACK = 2
+        const val MESSAGE_PREVIOUS_TRACK = 3
+        const val MESSAGE_PAUSE = 4
+        const val MESSAGE_PLAY = 5
+
+        const val STATE_PAUSED = 6
+        const val STATE_PLAYING = 7
+        const val STATE_TIMELINE_CHANGED = 8
+        const val STATE_NEXT_TRACK = 9
+        const val STATE_PREV_TRACK = 10
+    }
+
+    private lateinit var playerNotificationManager: PlayerNotificationManager
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var audioManager: AudioManager
+    private var audioBecomingNoisy = AudioBecomingNoisyReceiver()
+    private var audioBecomingNoisyIntentFilter: IntentFilter? = null
+
+    private val audioPlayer = AudioPlayer.getInstance(this)
+    private val playerEventListener = PlayerEventListener()
+
+    private val messenger = Messenger(IncomingHandler())
+    private val serviceClients = mutableListOf<Messenger>()
+
+    private var playbackFolderAbsolutePath: String = ""
+
+    override fun onCreate() {
+        super.onCreate()
+
+        audioPlayer.playbackEngine.addListener(playerEventListener)
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        audioBecomingNoisyIntentFilter = IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
+
+        playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
+                this,
+                CHANNEL_ID,
+                R.string.app_name,
+                NOTIFICATION_ID,
+                NotificationDescriptionAdapter())
+
+        playerNotificationManager.setFastForwardIncrementMs(0)
+        playerNotificationManager.setRewindIncrementMs(0)
+
+        playerNotificationManager.setNotificationListener(object : PlayerNotificationManager.NotificationListener {
+            override fun onNotificationStarted(notificationId: Int, notification: Notification) {
+                startForeground(notificationId, notification)
+            }
+
+            override fun onNotificationCancelled(notificationId: Int) {
+                stopSelf()
+            }
+        })
+
+        playerNotificationManager.setPlayer(audioPlayer.playbackEngine)
+        mediaSession = MediaSessionCompat(this, TAG)
+        mediaSession.isActive = true
+        playerNotificationManager.setMediaSessionToken(mediaSession.sessionToken)
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlayer(audioPlayer.playbackEngine, null)
+    }
+
+    override fun onBind(intent: Intent?) = messenger.binder
+
+    override fun onAudioFocusChange(focusChange: Int) {
+        when (focusChange) {
+            AudioManager.AUDIOFOCUS_GAIN -> audioPlayer.playbackEngine.playWhenReady = true
+            AudioManager.AUDIOFOCUS_LOSS -> audioPlayer.playbackEngine.playWhenReady = false
+            AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> audioPlayer.playbackEngine.playWhenReady = false
+        }
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        playbackFolderAbsolutePath = intent.getStringExtra(AudioTracksActivity.AUDIO_FOLDER_ABSOLUTE_PATH)
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaSession.release()
+        audioPlayer.stop()
+        mediaSessionConnector.setPlayer(null, null)
+        playerNotificationManager.setPlayer(null)
+        audioPlayer.playbackEngine.removeListener(playerEventListener)
+    }
+
+    private inner class AudioBecomingNoisyReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (audioPlayer.playbackEngine.playWhenReady) {
+                audioPlayer.playbackEngine.playWhenReady = false
+            }
+        }
+    }
+
+    private inner class PlayerEventListener : Player.EventListener {
+        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            val requestAudioFocus = audioManager.requestAudioFocus(this@AudioPlayerService,
+                    AudioManager.STREAM_MUSIC,
+                    AudioManager.AUDIOFOCUS_GAIN)
+
+            if (playbackState == Player.STATE_READY && playWhenReady) {
+                this@AudioPlayerService.registerReceiver(audioBecomingNoisy, audioBecomingNoisyIntentFilter)
+                audioPlayer.playbackEngine.playWhenReady = requestAudioFocus == AudioManager.AUDIOFOCUS_REQUEST_GRANTED
+            } else if (playbackState == Player.STATE_READY) {
+                this@AudioPlayerService.unregisterReceiver(audioBecomingNoisy)
+                audioManager.abandonAudioFocus(this@AudioPlayerService)
+            }
+            if (playbackState == Player.STATE_READY && !playWhenReady) {
+                serviceClients.forEach {
+                    sendMessage(it, STATE_PAUSED)
+                }
+            }
+            if (playbackState == Player.STATE_READY && playWhenReady) {
+                serviceClients.forEach {
+                    sendMessage(it, STATE_PLAYING)
+                }
+            }
+            if (playbackState == PlaybackStateCompat.STATE_SKIPPING_TO_NEXT) {
+                serviceClients.forEach {
+                    sendMessage(it, STATE_NEXT_TRACK)
+                }
+            }
+            if (playbackState == PlaybackStateCompat.STATE_SKIPPING_TO_PREVIOUS) {
+                serviceClients.forEach {
+                    sendMessage(it, STATE_PREV_TRACK)
+                }
+            }
+        }
+    }
+
+    private inner class NotificationDescriptionAdapter : PlayerNotificationManager.MediaDescriptionAdapter {
+
+        override fun getCurrentContentTitle(player: Player) = audioPlayer.track!!.title
+
+        override fun getCurrentContentText(player: Player) = audioPlayer.track!!.band
+
+        override fun getCurrentLargeIcon(player: Player,
+                                         callback: PlayerNotificationManager.BitmapCallback)
+                = BitmapFactory.decodeResource(resources, R.drawable.albumcoverxx)
+
+
+        override fun createCurrentContentIntent(player: Player): PendingIntent? {
+//            val window = audioPlayer.currentWindowIndex
+//            return createPendingIntent(window)
+            val startAudioTracksActivityIntent = Intent(baseContext, AudioTracksActivity::class.java).apply {
+                putExtra(AudioTracksActivity.AUDIO_FOLDER_ABSOLUTE_PATH, playbackFolderAbsolutePath)
+            }
+            return PendingIntent.getActivity(baseContext, 0 , startAudioTracksActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+    }
+
+    internal inner class IncomingHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MESSAGE_REGISTER_CLIENT -> {
+                    serviceClients += msg.replyTo
+                }
+                MESSAGE_UNREGISTER_CLIENT -> {
+                    serviceClients -= msg.replyTo
+                }
+                MESSAGE_NEXT_TRACK -> {
+                    audioPlayer.playNextInQueue()
+                }
+                MESSAGE_PREVIOUS_TRACK -> {
+                    audioPlayer.playPreviousInQueue()
+                }
+                MESSAGE_PAUSE -> {
+                    audioPlayer.pause()
+                }
+                MESSAGE_PLAY -> {
+                    val index = msg.arg1
+                    audioPlayer.playAtIndexInQueue(index)
+                }
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
+
+    private fun sendMessage(messenger: Messenger, what: Int, arg: Int = 0, obj: Any? = null) {
+        val msg = Message.obtain()
+        msg.what = what
+        msg.arg1 = arg
+        messenger.send(msg)
+    }
+
+}
