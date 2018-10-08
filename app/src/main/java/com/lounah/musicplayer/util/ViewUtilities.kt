e76d24a3f762922ae658d03c81d6832067bb43e5
Lounah <lounahxrussia@gmail.com>
2018-10-08 14:14:21 +0300

    the issue in dpToPx will be fixed later lol

--- app/src/main/java/com/lounah/musicplayer/util/ViewUtilities.kt
+++ app/src/main/java/com/lounah/musicplayer/util/ViewUtilities.kt
@@ -4,14 +4,13 @@ import android.content.Context
 import android.util.DisplayMetrics
 import java.lang.Math.round
 import android.util.TypedValue
-
+import kotlin.math.round
 
 
 class ViewUtilities private constructor() {
 
     companion object {
-        fun dpToPx(dp: Int, context: Context) : Int
-                = round(dp * context.resources.displayMetrics.xdpi / 160)
+        fun dpToPx(dp: Int, context: Context) : Int = Math.round(dp * context.resources.displayMetrics.xdpi / 160f)
 
         fun pxToDp(px: Int, context: Context)
                 = Math.round(px * DisplayMetrics.DENSITY_DEFAULT.toDouble()) / context.resources.displayMetrics.xdpi
