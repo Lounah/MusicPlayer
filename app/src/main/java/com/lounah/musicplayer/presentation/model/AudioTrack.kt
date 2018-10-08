e64b4c509202a8ff885d17c9da3c6bf4278dc111
Lounah <lounahxrussia@gmail.com>
2018-10-08 14:15:15 +0300

    AudioTrack model improvements -- now it contains path to media

--- app/src/main/java/com/lounah/musicplayer/presentation/model/AudioTrack.kt
+++ app/src/main/java/com/lounah/musicplayer/presentation/model/AudioTrack.kt
@@ -1,7 +1,17 @@
 package com.lounah.musicplayer.presentation.model
 
-data class AudioTrack(val title: String? = "",
-                      val band: String? = "",
+import android.net.Uri
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+
+data class AudioTrack(var title: String? = "",
+                      var band: String? = "",
                       val genre: String? = "",
-                      val duration: Int = 0,
-                      val albumCoverURL: String? = "")
+                      var duration: Int = 0,
+                      val albumCoverURL: String? = "",
+                      var isBeingPlayed: Boolean? = false,
+                      val path: String? = "")
+
+ fun audioTrackToMediaSource(track: AudioTrack, dataSourceFactory: DefaultDataSourceFactory): MediaSource
+        = ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(track.path))
