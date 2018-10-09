573af1ce37acdca4ff887e405bb3a5bbcd5da71e
Lounah <lounahxrussia@gmail.com>
2018-10-10 00:54:22 +0300

    code clean up -- unused imports removed

--- app/src/main/java/com/lounah/musicplayer/presentation/main/MainActivity.kt
+++ app/src/main/java/com/lounah/musicplayer/presentation/main/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.lounah.musicplayer.presentation.main
 
+import android.os.*
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import com.lounah.musicplayer.R
 import com.lounah.musicplayer.presentation.browsemedia.BrowseMediaFragment
-import com.lounah.musicplayer.presentation.model.AudioTrack
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottom_audio_view.currentTrack = AudioTrack("ffsdf", "safsafasf", duration = 192)
+        setUpDefaultFragment()
+    }
 
-        supportFragmentManager?.beginTransaction()?.replace(R.id.main_fragment_container, BrowseMediaFragment())?.commit()
+    private fun setUpDefaultFragment() {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, BrowseMediaFragment())
+                .commit()
     }
 }
