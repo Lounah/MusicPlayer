b18b43ff4fe123e9b869b1d7019464c8b93b0111
Lounah <lounahxrussia@gmail.com>
2018-10-10 00:56:52 +0300

    unused imports removed; added new methods

--- app/src/main/java/com/lounah/musicplayer/util/ViewUtilities.kt
+++ app/src/main/java/com/lounah/musicplayer/util/ViewUtilities.kt
@@ -1,22 +1,41 @@
 package com.lounah.musicplayer.util
 
 import android.content.Context
+import android.content.res.Configuration
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
 import android.util.DisplayMetrics
-import java.lang.Math.round
 import android.util.TypedValue
-import kotlin.math.round
+import android.view.MotionEvent
+import kotlin.math.abs
 
 
-class ViewUtilities private constructor() {
+object ViewUtilities {
 
-    companion object {
-        fun dpToPx(dp: Int, context: Context) : Int = Math.round(dp * context.resources.displayMetrics.xdpi / 160f)
+    fun dpToPx(dp: Int, context: Context): Int = Math.round(dp * (context.resources.displayMetrics.densityDpi / 160f))
 
-        fun pxToDp(px: Int, context: Context)
-                = Math.round(px * DisplayMetrics.DENSITY_DEFAULT.toDouble()) / context.resources.displayMetrics.xdpi
+    fun pxToDp(px: Int, context: Context) = Math.round(px * DisplayMetrics.DENSITY_DEFAULT.toDouble()) / context.resources.displayMetrics.xdpi
 
-        fun spToPx(sp: Float, context: Context): Int {
-            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.resources.displayMetrics).toInt()
-        }
+    fun spToPx(sp: Float, context: Context): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.resources.displayMetrics).toInt()
     }
+
+    fun isMotionEventInRect(targetRect: Rect, motionEvent: MotionEvent) = motionEvent.x >= targetRect.left - 20f
+            && motionEvent.x <= targetRect.right + 20f
+            && motionEvent.y >= targetRect.top - 20f
+            && motionEvent.y <= targetRect.bottom + 20f
+
+    fun isMotionEventInRect(targetRect: RectF, motionEvent: MotionEvent) = motionEvent.x >= targetRect.left - 20f
+            && motionEvent.x <= targetRect.right + 20f
+            && motionEvent.y >= targetRect.top - 20f
+            && motionEvent.y <= targetRect.bottom + 20f
+
+    fun drawOnClickShape(canvas: Canvas, targetRect: Rect, context: Context, paint: Paint) {
+        canvas.drawCircle(targetRect.exactCenterX(), targetRect.exactCenterY(), abs(targetRect.right - targetRect.left) / 2f + dpToPx(8, context), paint)
+    }
+
+    fun isInLandscape(context: Context)
+            = context.resources.configuration.orientation ==  Configuration.ORIENTATION_LANDSCAPE
 }
